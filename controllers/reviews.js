59c91c322528c7f59a369e0c2435004c020979da
James Haff <jameshaff@Jamess-MacBook-Pro.local>
2022-04-01 15:52:43 -0500

    Added final code

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,3 +1,5 @@
+// this is our movie model, which can talk to the database
+const Movie = require('../models/movie')
 
 module.exports = {
 	create
@@ -7,11 +9,28 @@ function create(req, res){
 	// two things we need to know from the request?
 	// req.params.id = id of the movie we want to tadd the review to
 	// req.body - contents of the form, which is the review we want to add to the movie
-	
+	// console.log(req.body)
+	// console.log(req.params.id)
+	// res.send('hello im hitting the create route in the reviews change me later')
 	// Find the movie from the database
+	// Movie.findById is a mongoose Method
+	Movie.findById(req.params.id, function(err, movieFromTheDatabase) {
+		// add the review (req.body) to the movieFromTheDatabase
+		movieFromTheDatabase.reviews.push(req.body); // mutating a document 
+		// we have to tell mongodb we changed the document, 
+		// tell mongodb by save
+		movieFromTheDatabase.save(function(err){
+			console.log(movieFromTheDatabase)
+			// then we want to respond to the client!
+			 // redirect them to a page, What page makes sense to redirect?
+			res.redirect(`/movies/${movieFromTheDatabase._id}`)	
+		})
 
-	// add the review (req.body) to the movieFromTheDatabase
-
-	// then we want to respond to the client! // redirect them to a page, What page makes sense to redirect?
+	})
+		
+	// res.send('hello')
+	
+	// 
+	
 
 }
